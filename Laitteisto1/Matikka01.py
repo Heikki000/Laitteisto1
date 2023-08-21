ab2aac62c811120a80fb68fa26fea8d62dda9ef5
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-08-21 12:44:51 +0300

    2

--- Laitteisto1/Matikka01.py
+++ Laitteisto1/Matikka01.py
@@ -1,1 +1,22 @@
-"tehtava"
+"tehtava 2"
+import math
+
+vastaus_1a = math.degrees(2.493)
+vastaus_1b = math.degrees(0.911)
+
+vastaus_2a = math.radians(137.7)
+vastaus_2b = math.radians(62.3)
+
+taulukko_degree = [30, 45, 60, 90, 120, 135, 150, 180, 270, 360]
+taulukko_radians = []
+
+for n in taulukko_degree:
+    radiaani = math.radians(n)
+    taulukko_radians.append(radiaani)
+
+print(vastaus_1a)
+print(vastaus_1b)
+print(vastaus_2a)
+print(vastaus_2b)
+print(taulukko_radians)
+
