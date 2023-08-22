d43e2457e0730e3b7fceaf53aab649b21b10cf68
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-08-22 22:43:26 +0300

    5

--- Laitteisto1/Matikka01.py
+++ Laitteisto1/Matikka01.py
@@ -8,21 +8,30 @@ vastaus_2a = math.radians(137.7)
 vastaus_2b = math.radians(62.3)
 
 taulukko_degree = [30, 45, 60, 90, 120, 135, 150, 180, 270, 360]
-taulukko_radians = {}
+sanakirja_radians = {}
 
 for n in taulukko_degree:
     radiaani = math.radians(n)
-    taulukko_radians[n] = f"{radiaani:.2f}"
+    sanakirja_radians[n] = f"{radiaani:.2f}"
 
-print(taulukko_radians)
+hypotenuusa = math.hypot(2.3, 4.7)
+kulma_1 = math.degrees(math.atan(2.3/4.7))
+kulma_2 = math.degrees(math.atan(4.7/2.3))
+
+taulukko_number = [0, 4, 7, 15]
+sanakirja_factorial = {}
+
+for n in taulukko_number:
+    factorial_n = math.factorial(n)
+    sanakirja_factorial[n] = f"{factorial_n}"
 
 print(vastaus_1a)
 print(vastaus_1b)
 print(vastaus_2a)
 print(vastaus_2b)
-print(taulukko_radians)
-
-hypotenuusa = math.hypot(2.3, 4.7)
-print(hypotenuusa)
+print(sanakirja_radians)
+print(f"Hypotenuusa on {hypotenuusa:.1f} ja 2 kulmaa ovat {kulma_1:.1f} ja {kulma_2:.1f}.")
+for key, value in sanakirja_factorial.items():
+    print(f"{key}! : {value}")
 
 
