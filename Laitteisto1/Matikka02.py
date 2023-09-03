b09f2ad6b07192369c9d868bf7661586648aeb55
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-09-03 12:11:27 +0300

    2

--- Laitteisto1/Matikka02.py
+++ Laitteisto1/Matikka02.py
@@ -3,4 +3,11 @@ import numpy as np
 #1
 lukutaulukko = np.array([1, 5, 11, 18, 100])
 print(lukutaulukko)
+'''
+print("lukutaulukko -> ndim:", lukutaulukko.ndim)
+print("lukutaulukko -> shape:", lukutaulukko.shape)
+print("lukutaulukko -> size:", lukutaulukko.size)
+print("lukutaulukko -> dtype:", lukutaulukko.dtype)
+'''
+#2
 
