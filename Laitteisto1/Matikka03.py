bc6a90f89a2e192812878081c463ec1dfe2f95d9
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-09-04 11:41:44 +0300

    valmis

--- Laitteisto1/Matikka03.py
+++ Laitteisto1/Matikka03.py
@@ -1,1 +1,15 @@
 #Laitteisto1 / Matikka, tehtava 3
+import numpy as np
+import numpy.linalg as la
+import matplotlib.pyplot as plt
+
+#teht. 1
+aa = np.array([[-1, 2], [3, -5]])
+bb = np.array([[2, 0], [-1, 4]])
+print(2*aa+3*bb)
+print(aa-bb)
+
+#teht. 2
+aaa = np.array([[1+1j, 3, 1j], [0, 5+1j, 1j]])
+ccc = (1+1j) * aaa
+print(ccc)
