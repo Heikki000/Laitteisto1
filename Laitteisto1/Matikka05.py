b27b4822eef17529c8ba108506aac959fb8e1bfa
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-09-19 19:50:10 +0300

    valmis(?)2

--- Laitteisto1/Matikka05.py
+++ Laitteisto1/Matikka05.py
@@ -34,12 +34,12 @@ print(tulo_aaaa)
 #tehtava 2
 print()
 print("Tehtava 2 / transpoosit tehtavasta 1")
-#transpoosit
+#transpoosit    print(12345.T)       /toinen merkintätapa
 #A
 qq = np.array([[4, 9, 0], [-3, 7, -11]])
 print(np.transpose(qq))
 print()
-# print(tulo_a.T)       /toinen merkintätapa
+
 #B
 ww = np.array([[8, 9], [-3, 12], [0, -1], [7, 1]])
 print(np.transpose(ww))
@@ -53,15 +53,15 @@ ee = np.array([[5, -6], [8, 10]])
 print(int(la.det(ee)))
 #b)
 eee = np.array([[1 - 1j, -1j], [1j, 1 - 1j]])
-print(np.round(la.det(eee), decimals=0))
+print(la.det(eee))
 print()
 print("Tehtava 2 / determinantit tehtavasta 2.2")
 #a)
 eeee = np.array([[2, 3, 4], [-2, -1, 1], [5, 3, 2]])
-print(int(la.det(eeee)))
+print(np.round(la.det(eeee), decimals=0))
 #b)
 eeeee = np.array([[3, 15, 7], [0, -4, 0], [3, 2, 3]])
-print(int(la.det(eeeee)))
+print(np.round(la.det(eeeee), decimals=0))
 print()
 print("Tehtava 2 / determinantti tehtavasta 3")
 rr = np.array([[-2, 0, 8, 5], [3, -1, 2, 1], [4, 7, 6, 2], [1, 0, 2, 3]])
