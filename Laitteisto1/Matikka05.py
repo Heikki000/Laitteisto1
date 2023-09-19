6dd764e11486112abe02333a6bd36c966da5f95e
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-09-19 19:21:19 +0300

    valmis(?)

--- Laitteisto1/Matikka05.py
+++ Laitteisto1/Matikka05.py
@@ -8,14 +8,14 @@ yksi_b = np.array([[0, 1, 3], [2, -3, 5]])
 tulo_a = np.dot(yksi_a, yksi_b)
 print("Tehtava 1")
 print(tulo_a)
-
+print()
 #b)
 
 yksi_aa = np.array([[1, 3, 5], [0, -2, 1], [2, -1, 4]])
 yksi_bb = np.array([[1], [-3], [-1]])
 tulo_aa = np.dot(yksi_aa, yksi_bb)
 print(tulo_aa)
-
+print()
 
 #c)
 
@@ -23,7 +23,7 @@ yksi_aaa = np.array([[2, 0, 1], [1, -3, 4], [0, 1, 5]])
 yksi_bbb = np.array([[3], [-5], [7]])
 tulo_aaa = np.dot(yksi_aaa, yksi_bbb)
 print(tulo_aaa)
-
+print()
 #d)
 
 yksi_aaaa = np.array([[1, -4, 2], [3, 0, -2], [2, 1, 0]])
@@ -33,25 +33,36 @@ print(tulo_aaaa)
 
 #tehtava 2
 print()
-print("Tehtava 2. transpoosit 1")
+print("Tehtava 2 / transpoosit tehtavasta 1")
 #transpoosit
 #A
-qq = np.array([4, 9, 0], [-3, 7, -11])
+qq = np.array([[4, 9, 0], [-3, 7, -11]])
 print(np.transpose(qq))
+print()
 # print(tulo_a.T)       /toinen merkintätapa
 #B
 ww = np.array([[8, 9], [-3, 12], [0, -1], [7, 1]])
 print(np.transpose(ww))
 print()
-print("Tehtava 2. determinantit 1")
+print("Tehtava 2 / determinantit tehtavasta 2.1")
 #determinantit          /numpy.linalg.det()
 print()
-print("Tehtava 2. determinantit")
+
 #a)
-#print(la.det(tulo_a))      /ei voi laskea, koska ei ole neliömatriisi
+ee = np.array([[5, -6], [8, 10]])
+print(int(la.det(ee)))
 #b)
-#print(la.det(tulo_aa))      /ei voi laskea, koska ei ole neliömatriisi
-#c)
-#print(la.det(tulo_aaa))     /ei voi laskea, koska ei ole neliömatriisi
-#d)
-print(la.det(tulo_aaaa))
+eee = np.array([[1 - 1j, -1j], [1j, 1 - 1j]])
+print(np.round(la.det(eee), decimals=0))
+print()
+print("Tehtava 2 / determinantit tehtavasta 2.2")
+#a)
+eeee = np.array([[2, 3, 4], [-2, -1, 1], [5, 3, 2]])
+print(int(la.det(eeee)))
+#b)
+eeeee = np.array([[3, 15, 7], [0, -4, 0], [3, 2, 3]])
+print(int(la.det(eeeee)))
+print()
+print("Tehtava 2 / determinantti tehtavasta 3")
+rr = np.array([[-2, 0, 8, 5], [3, -1, 2, 1], [4, 7, 6, 2], [1, 0, 2, 3]])
+print(int(la.det(rr)))
